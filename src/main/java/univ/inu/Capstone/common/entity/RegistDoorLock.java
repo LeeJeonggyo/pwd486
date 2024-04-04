72c298d599deebb61972a67430ce4d826cf6626c
jg <ella.code.j@gmail.com>
2024-04-04 15:50:21 +0900

    Mod : 주석 추가

--- src/main/java/univ/inu/Capstone/common/entity/RegistDoorLock.java
+++ src/main/java/univ/inu/Capstone/common/entity/RegistDoorLock.java
@@ -17,7 +17,7 @@ public class RegistDoorLock extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long rdlSeq;    // 구분자
     private String rdlName; // 카드키 이름
-    private int rdlAuth;    // 권한 (1:OWNER / 2:MEMBER)
+    private int rdlAuth;    // 권한 (1:OWNER / 2:MEMBER / 3:GUEST)
     private int rdlApprove; // 승인여부 (0: 미승인 / 1: 승인) - OWNER 권한은 무조건 1
 
     @ManyToOne
